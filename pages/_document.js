979c757a54f16eec3d9b5fa7ba6653af72bdb9fe
Cynthia Ardman <cardman@appomni.com>
2025-07-18 16:44:55 -0700

    Add server-side loading for netrun puzzles

--- pages/_document.js
+++ pages/_document.js
@@ -14,20 +14,7 @@ class MyDocument extends Document {
           <Main />
           <NextScript />
 
-          {/* SimpleAnalytics */}
-          <script
-            async
-            defer
-            data-collect-dnt="true"
-            src="https://sa.cyberpunk-hacker.com/latest.js"
-          ></script>
-          <noscript>
-            <img
-              src="https://sa.cyberpunk-hacker.com/noscript.gif?collect-dnt=true"
-              alt=""
-              referrerPolicy="no-referrer-when-downgrade"
-            />
-          </noscript>
+          {/* SimpleAnalytics removed to prevent external script issues */}
         </body>
       </Html>
     );
