5748ed91b5d89ff7091df611953103f09efe1238
Cynthia Ardman <cardman@appomni.com>
2025-07-18 17:36:21 -0700

    Add failure overlay style

--- pages/puzzle.tsx
+++ pages/puzzle.tsx
@@ -663,7 +663,7 @@ export default function PuzzlePage() {
           </div>
         )}
         {ended && solved.size !== puzzle.daemons.length && (
-          <div className={styles["terminal-overlay"]}>
+          <div className={`${styles["terminal-overlay"]} ${styles.failure}`}> 
             <pre className={styles["terminal-log"]}>{logLines.join("\n")}</pre>
             {logLines.length ===
               generateFailureLog(solved.size, puzzle.daemons.length).length && (
